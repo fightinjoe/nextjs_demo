69314af8083f9170732f52c9962ee8350ed7afa0
Aaron Wheeler <aaron@fightinjoe.com>
2023-07-05 12:45:21 -0400

    Added server component call to Google Sheets

--- app/page.js
+++ app/page.js
@@ -1,88 +1,40 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-export default function Home() {
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+import { JWT } from 'google-auth-library';
+
+export default async function Home() {
+
+  // Returns the sheet and rows for the given title
+  async function loadSheetAndRows( title ) {
+    const sheet = doc.sheetsByTitle[ title ];
+    const rows = await sheet.getRows();
+
+    return [sheet, rows];
+  }
+
+  const SCOPES = [
+    'https://www.googleapis.com/auth/spreadsheets'
+  ]
+  
+  const serviceAccountAuth = new JWT({
+    email: process.env.GAPI_CLIENT_EMAIL,
+    key: process.env.GAPI_PRIVATE_KEY,
+    scopes: SCOPES,
+  });
+  
+  const doc = new GoogleSpreadsheet(process.env.SPREADSHEET_ID, serviceAccountAuth);
+  await doc.loadInfo();
+
+  let [,sellerRows] = await loadSheetAndRows('Sellers');
+  let [productSheet,productRows] = await loadSheetAndRows('Products');
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <h1>Ozeriany Property Owners Association</h1>
-        <p>PO Box 417, Eldred, NY 12737</p>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <h1>{`${sellerRows.length} sellers`}</h1>
+        <h2>{`${productRows.length} products`}</h2>
       </div>
     </main>
   )
