a88efb3401a88b0041259decba39ad1cda8e0817
Aaron Wheeler <aaron@fightinjoe.com>
2023-07-12 09:53:16 -0400

    Modularized data formatting

--- app/mobile/results.js
+++ app/mobile/results.js
@@ -5,50 +5,11 @@ import { useState } from 'react';
 import styles from './page.module.css'
 import Seller, { Product } from './seller';
 
-export default function Results({ sellers }) {
-  sellers = JSON.parse(sellers);
+export default function Results({ products }) {
+  products = JSON.parse(products);
 
   let [page, setPage] = useState(0);
   let [search, setSearch] = useState('');
-
-  // Format the price to look like $1.00  
-  const formatPrice = (price) => {
-    price = price.replace('$', '');
-
-    let pieces = [ ...price.split('.'), '00' ];
-    pieces[1] = pieces[1].padEnd(2, '0');
-
-    return `$${ pieces[0] }.${ pieces[1].slice(0,2) }`;
-  }
-
-  // Arrange the product array for easier printing
-  let products = sellers.map( seller => {
-    return seller.products
-      // add the seller info to each product
-      .map( product => {
-        let { url, avatar_src, store, owner } = seller;
-        product.seller = { url, avatar_src, store, owner };
-
-        // Cleanup the price
-        product.price = formatPrice(product.price);
-
-        // Remove long URLs that wreck description formatting
-        let d = product.description || '';
-        d = d
-          .replace(/<[^>]+>/g, '')
-          .replace(/\S{21,}/g, '');
-
-        product.description = d;
-
-        // Add a search string for easier searching
-        product.search = `${ product.title } ${ product.description } ${ product.seller.store } ${ product.seller.owner }`;
-
-        return product;
-      });
-  })
-
-  // flatten to a 1 dimensional array of products
-  products = products.reduce( (a, b) => a.concat(b) );
 
   if (search) {
     products = products
--- app/page.js
+++ app/page.js
@@ -1,75 +1,13 @@
 // import Image from 'next/image'
 import styles from './page.module.css'
 
-import { GoogleSpreadsheet } from 'google-spreadsheet';
-import { JWT } from 'google-auth-library';
+import { getSellerData } from '../lib/data';
 
 import Results from './results';
 
 export default async function Home() {
 
-  // Returns the sheet and rows for the given title
-  async function loadSheetAndRows( title ) {
-    const sheet = doc.sheetsByTitle[ title ];
-    const rows = await sheet.getRows();
-
-    return [sheet, rows];
-  }
-
-  const SCOPES = [
-    'https://www.googleapis.com/auth/spreadsheets'
-  ]
-  
-  const serviceAccountAuth = new JWT({
-    email: process.env.GAPI_CLIENT_EMAIL,
-    key: process.env.GAPI_PRIVATE_KEY,
-    scopes: SCOPES,
-  });
-  
-  const doc = new GoogleSpreadsheet(process.env.SPREADSHEET_ID, serviceAccountAuth);
-  await doc.loadInfo();
-
-  let [,sellerRows] = await loadSheetAndRows('Sellers');
-  let [,productRows] = await loadSheetAndRows('Products');
-
-  let sellers = sellerRows.map( row => {
-    let seller = {
-      avatar_src: row.get('avatar_src'),
-      owner: row.get('owner'),
-      username: row.get('username'),
-      store: row.get('store'),
-      location: row.get('location')
-    };
-
-    let products = productRows
-      .filter( product => product.get('username') === row.get('username') )
-      .map( prow => ({
-        link_href: prow.get('link_href'),
-        img_src: prow.get('img_src'),
-        title: prow.get('title'),
-        price: prow.get('price'),
-        description: prow.get('description'),
-      }));
-    
-    seller.product_count = products.length;
-    seller.products = products.slice(0,6);
-
-    return seller;
-  });
-
-  sellers = sellers.map( seller => {
-    seller.search = [
-      seller.owner,
-      seller.username,
-      seller.store,
-      seller.products.map( product => ([
-        product.title,
-        product.description
-      ].join("\n")))
-    ].join("\n");
-
-    return seller;
-  });
+  const sellers = await getSellerData();
 
   return (
     <main className={styles.main}>
